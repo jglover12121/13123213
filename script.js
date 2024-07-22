5755ad34847e69b2f197888dab4ff2cc40adc1af
greatjosh2007 <26950556-greatjosh2007@users.noreply.replit.com>
2024-07-22 05:28:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const save = document.getElementById("save");
+const clear = document.getElementById("clear");
+const download = document.getElementById("download");
+const input = document.getElementById("input");
+const text = document.getElementById("text");
+
+if(localStorage.getItem("input") != undefined){
+  text.innerHTML = localStorage.getItem("input");
+}
+
+let isSure = false;
+
+save.addEventListener("click", ()=>{
+  if(localStorage.getItem("input") == text.innerHTML && input.value){
+    return;
+  }
+  if(isSure == false){
+    isSure = true;
+    save.innerHTML = "Are you sure?";
+    return;
+  }
+  isSure = false;
+  try{
+    localStorage.setItem("input",input.value);
+  }catch(err){
+    console.log(err)
+  } finally{
+    text.innerHTML = localStorage.getItem("input");
+  }
+})
+
+clear.addEventListener("click", ()=>{
+  localStorage.removeItem("input");
+  window.location.reload();
+})
